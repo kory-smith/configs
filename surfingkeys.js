4c3aa2a046d5d883312ba4acede0d065d063b937
Kory Smith <kor54e@gmail.com>
2021-09-27 19:28:06 -0400

    Comment out all unused bindings

--- surfingkeys.js
+++ surfingkeys.js
@@ -20,24 +20,24 @@ map('F', 'gf');
 map('sf', 'af');
 
 // K will move to the tab to the right
-map('K', 'R');
+// map('K', 'R');
 
 // J will move to the tab to the left
-map('J', 'E');
+// map('J', 'E');
 
 // H will move back one history entry
 map('H', 'S');
 
 // L will move forward one history entry
-map('L', 'D');
+// map('L', 'D');
 
 unmap('p');
 
-mapkey('p', "Open the clipboard's URL in the current tab", function() {
-    Clipboard.read(function(response) {
-        window.location.href = response.data;
-    });
-});
+// mapkey('p', "Open the clipboard's URL in the current tab", function() {
+//     Clipboard.read(function(response) {
+//         window.location.href = response.data;
+//     });
+// });
 
 mapkey(',', '#0enter ephemeral PassThrough mode to temporarily suppress SurfingKeys', function() {
     Normal.passThrough(1000);
@@ -49,9 +49,9 @@ mapkey('gxt', '#3Close tab on right', function() {
 mapkey('gxT', '#3Close tab on left', function () {
     RUNTIME("closeTabLeft"); }); 
     
-mapkey('T', 'Choose a tab with omnibar', function() {
-    Front.openOmnibar({type: "Tabs"});
-});    
+// mapkey('T', 'Choose a tab with omnibar', function() {
+//     Front.openOmnibar({type: "Tabs"});
+// });    
 
 mapkey('P', '#7Open clipboard in new tab', function() {
     Clipboard.read(function(response) {
@@ -59,39 +59,39 @@ mapkey('P', '#7Open clipboard in new tab', function() {
         });
     });
 
-unmap('gn');
+// unmap('gn');
 
-mapkey('gny', '#3Open Yext in New Tab', function(){
-    tabOpenLink("https://www.yext.com/s/1029251/entities?entityGroupId=0")}, 
-                {tabbed: true, active: true});
+// mapkey('gny', '#3Open Yext in New Tab', function(){
+//     tabOpenLink("https://www.yext.com/s/1029251/entities?entityGroupId=0")}, 
+//                 {tabbed: true, active: true});
 
-mapkey('gnk', '#3Open Kroger in New Tab', function(){
-    tabOpenLink("https://www.kroger.com")}, 
-                {tabbed: true, active: true});
+// mapkey('gnk', '#3Open Kroger in New Tab', function(){
+//     tabOpenLink("https://www.kroger.com")}, 
+//                 {tabbed: true, active: true});
 
-mapkey('gnb', '#3Open YNAB in New Tab', function(){
-    tabOpenLink("https://app.youneedabudget.com/9becfa68-4c59-4c2f-888d-617cb9416273/budget")}, 
-                {tabbed: true, active: true});
+// mapkey('gnb', '#3Open YNAB in New Tab', function(){
+//     tabOpenLink("https://app.youneedabudget.com/9becfa68-4c59-4c2f-888d-617cb9416273/budget")}, 
+//                 {tabbed: true, active: true});
 
-mapkey('gnj', '#3Open Jira in New Tab', function(){
-    tabOpenLink("https://jira.kroger.com/jira/secure/RapidBoard.jspa?rapidView=7037")}, 
-                {tabbed: true, active: true});
+// mapkey('gnj', '#3Open Jira in New Tab', function(){
+//     tabOpenLink("https://jira.kroger.com/jira/secure/RapidBoard.jspa?rapidView=7037")}, 
+//                 {tabbed: true, active: true});
 
-mapkey('gns', '#3Open Search Console in New Tab', function(){
-    tabOpenLink("https://search.google.com/u/1/search-console?resource_id=https://www.kroger.com/&hl=en")}, 
-                {tabbed: true, active: true});
+// mapkey('gns', '#3Open Search Console in New Tab', function(){
+//     tabOpenLink("https://search.google.com/u/1/search-console?resource_id=https://www.kroger.com/&hl=en")}, 
+//                 {tabbed: true, active: true});
 
 
-addSearchAliasX('k', 'Kroger', 'https://www.kroger.com/search?query=');
-addSearchAliasX('y', 'Yext', 'https://www.yext.com/s/1029251/entities?entityGroupId=0#search=');
+// addSearchAliasX('k', 'Kroger', 'https://www.kroger.com/search?query=');
+// addSearchAliasX('y', 'Yext', 'https://www.yext.com/s/1029251/entities?entityGroupId=0#search=');
 
-mapkey('oy', '#8Open Search with alias y', function() {
-    Front.openOmnibar({type: "SearchEngine", extra: "y"});
-});
+// mapkey('oy', '#8Open Search with alias y', function() {
+//     Front.openOmnibar({type: "SearchEngine", extra: "y"});
+// });
 
-mapkey('ok', '#8Open Search with alias k', function() {
-    Front.openOmnibar({type: "SearchEngine", extra: "k"});
-});
+// mapkey('ok', '#8Open Search with alias k', function() {
+//     Front.openOmnibar({type: "SearchEngine", extra: "k"});
+// });
 
 aceVimMap('jk', '<Esc>', 'insert');
 aceVimMap('kj', '<Esc>', 'insert');

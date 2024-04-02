d5a17dd543f5df8a7cb473467a409f645d7ca8c5
Kory Smith <kor54e@gmail.com>
2024-04-02 17:00:49 -0400

    uses bun shell for the script and gets espanso stuff set up

---
+++ setup.ts
@@ -0,0 +1,73 @@
+import { $ } from "bun";
+
+// /Users/ks61347/Gits/configs/setup.ts -> /Users/ks61347/Gits/configs
+const absolutePath = import.meta.path.split("/").slice(0, -1).join("/");
+
+async function setupEspanso(isDryRun: boolean): Promise<void> {
+  console.log("Setting up Espanso configs...");
+  const espansoConfig = await $`espanso path`.text();
+  const configDirPath = espansoConfig.split("\n")[0].split(":")[1].trim();
+
+  console.log({ configDirPath });
+
+  const { exitCode } = await $` find -d ${configDirPath}`.nothrow().quiet();
+  const configDirExists = exitCode === 0;
+
+  const matchesPath = "match/packages/base.yml";
+  if (configDirExists) {
+    const { exitCode: matchesExitCode } =
+      await $`find ${configDirPath}/${matchesPath}`.quiet();
+    const matchesFileExists = matchesExitCode === 0;
+    if (!matchesFileExists) {
+      await $`ln -s ${absolutePath}/espanso_matches.yml ${configDirPath}/${matchesPath}`;
+    } else {
+      // Otherwise it DOES exist
+      const matchesFileIsASymLink =
+        (await $`test -L ${configDirPath}/${matchesPath}`.nothrow().quiet())
+          .exitCode === 0;
+      if (matchesFileIsASymLink) {
+        console.log("Matches file is already a symlink. Skipping...");
+        // Otherwise, it's a static file
+      } else {
+        await $`rm ${configDirPath}/${matchesPath}`;
+        await $`ln -s ${absolutePath}/espanso_matches.yml ${configDirPath}/${matchesPath}`;
+      }
+    }
+
+    const configPath = "config/default.yml";
+    const { exitCode: configExitCode } =
+      await $`find ${configDirPath}/${matchesPath}`.quiet();
+    const configFileExists = configExitCode === 0;
+    if (!configFileExists) {
+      await $`ln -s ${absolutePath}/espanso_config.yml ${configDirPath}/${configPath}`;
+    } else {
+      // Otherwise it DOES exist
+      const configFileIsASymLink =
+        (await $`test -L ${configDirPath}/${configPath}`.nothrow().quiet())
+          .exitCode === 0;
+      if (configFileIsASymLink) {
+        console.log("Config file is already a symlink. Skipping...");
+        // Otherwise, it's a static file
+      } else {
+        await $`rm ${configDirPath}/${configPath}`;
+        await $`ln -s ${absolutePath}/espanso_config.yml ${configDirPath}/${configPath}`;
+      }
+    }
+  }
+}
+
+async function setupZsh(isDryRun: boolean): Promise<void> {
+  // todo
+}
+
+async function main() {
+  console.log("Setting up your project...");
+
+  const isDryRun = Bun.argv.includes("--dry-run");
+
+  await setupEspanso(isDryRun);
+
+  await setupZsh(isDryRun);
+}
+
+await main();

f793c5d732bd6c5d1b7eb35cda7f79d0a257a97b
Kory Smith <kor54e@gmail.com>
2024-11-13 11:22:02 -0500

    removes a ton of unused keys from surfingkeys config

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,24 +1,13 @@
 // https://raw.githubusercontent.com/kory-smith/configs/master/surfingkeys.js
 const {
-    aceVimMap,
-    mapkey,
-    imap,
-    imapkey,
-    unmapAllExcept,
-    getClickableElements,
-    vmapkey,
-    map,
-    unmap,
-    cmap,
-    addSearchAlias,
-    removeSearchAlias,
-    tabOpenLink,
-    readText,
-    Clipboard,
-    Front,
-    Hints,
-    Visual,
-    RUNTIME
+  aceVimMap,
+  mapkey,
+  map,
+  unmap,
+  tabOpenLink,
+  Clipboard,
+  Hints,
+  RUNTIME,
 } = api;
 
 settings.hintAlign = "left";
@@ -27,7 +16,7 @@ settings.focusFirstCandidate = true;
 //Events.hotKey="<Meta-s>";
 
 // Preserve the "don't activate on this site" funcionality on Mac.
-map('<Ctrl-i>', '<Alt-s>');
+map("<Ctrl-i>", "<Alt-s>");
 
 // an example to create a new mapping `ctrl-y`
 /*mapkey('<Ctrl-y>', 'Show me the money', function() {
@@ -36,52 +25,90 @@ map('<Ctrl-i>', '<Alt-s>');
 */
 
 // F will open a link in a new tab, unfocused.
-map('F', 'gf');
+map("F", "gf");
 
 // sf will open a link in a new tab, focused.
-map('sf', 'af');
+map("sf", "af");
 
-map(';u', ';U');
+map(";u", ";U");
 
 // H will hover over links
-map('H', '<Ctrl-h>');
+map("H", "<Ctrl-h>");
 
-unmap('p');
-// Remove "x" to close tab
-unmap('x');
+const keysToUnmap = [
+  "x",
+  "o",
+  "B",
+  "S",
+  "r",
+  "D",
+  "s",
+  "p",
+  "P",
+  "on",
+  "E",
+  "R",
+  "T",
+  "t",
+  "m",
+  "cc",
+  "cq",
+  ";t",
+  "q",
+  "sg",
+  "sd",
+  "sb",
+  "se",
+  "sw",
+  "ss",
+  "sh",
+  "sy",
+  ";pp",
+  ";pj",
+  ";pf",
+  ";cq",
+  "X",
+  "ZZ",
+  "ZR",
+];
+keysToUnmap.forEach((key) => {
+  unmap(key);
+});
 
-
-mapkey(',', '#0enter ephemeral PassThrough mode to temporarily suppress SurfingKeys', function() {
+mapkey(
+  ",",
+  "#0enter ephemeral PassThrough mode to temporarily suppress SurfingKeys",
+  function () {
     Normal.passThrough(1000);
+  }
+);
+
+mapkey("gxt", "#3Close tab on right", function () {
+  RUNTIME("closeTabRight");
 });
 
-mapkey('gxt', '#3Close tab on right', function() {
-    RUNTIME("closeTabRight");});
-
-mapkey('gxT', '#3Close tab on left', function () {
-    RUNTIME("closeTabLeft"); }); 
-    
-mapkey('P', '#7Open clipboard in new tab', function() {
-    Clipboard.read(function(response) {
-      tabOpenLink(response.data);
-        });
-    });
-
-mapkey('yY', '#1Copy all tabs url', function() {
-    RUNTIME('getTabs', null, function (response) {
-        Clipboard.write([window.location.href].concat(response.tabs.map(tab => tab.url)).join('\n'))
-    })
+mapkey("gxT", "#3Close tab on left", function () {
+  RUNTIME("closeTabLeft");
 });
 
-mapkey("q", "#7View image in new tab", function() {
-    Hints.create("img", i => tabOpenLink(i.src));
+mapkey("yY", "#1Copy all tabs url", function () {
+  RUNTIME("getTabs", null, function (response) {
+    Clipboard.write(
+      [window.location.href]
+        .concat(response.tabs.map((tab) => tab.url))
+        .join("\n")
+    );
   });
-  
-// Map 'H' and 'L' to move to the beginning/end of lines in Ace editor
-aceVimMap('H', '0', 'normal');
-aceVimMap('L', '$', 'normal');
+});
 
-unmapAllExcept(['f', 'sf', '.'], /reddit.com/)
+mapkey("q", "#7View image in new tab", function () {
+  Hints.create("img", (i) => tabOpenLink(i.src));
+});
+
+// Map 'H' and 'L' to move to the beginning/end of lines in Ace editor
+aceVimMap("H", "0", "normal");
+aceVimMap("L", "$", "normal");
 
 // For more information about using this blacklist, visit https://github.com/brookhong/Surfingkeys/issues/63
-settings.blacklistPattern = /.*mail.google.com.*|.*reddit.com.*|.*todoist.com.*|.*drive.google.com.*|.*twitter.com.|.*github.dev.*/i
+settings.blacklistPattern =
+  /.*mail.google.com.*|.*reddit.com.*|.*todoist.com.*|.*drive.google.com.*|.*twitter.com.|.*github.dev.*/i;
